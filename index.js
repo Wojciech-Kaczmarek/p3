cfadaaeb43ebe694fdd6ab4771303dc68bc275f8
Jakub Pusiak <jakub.pusiak@gmail.com>
2018-07-26 21:32:58 +0200

    Create form and jQuery variables.

--- index.js
+++ index.js
@@ -4,3 +4,9 @@ const changeColor = () => {
     $dupsko.toggleClass("red_n1");
 };
 $button_n1.click(changeColor);
+
+// $ === jQuery
+const $input = $("#input");
+const $output = $("#output");
+
+// TODO: handle input to show value below
