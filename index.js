8419f7ed02925c739cddf8ddefb95f29eefd7aa6
Jakub Pusiak <jakub.pusiak@gmail.com>
2018-07-26 23:00:48 +0200

    Configure React.

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return <div>Hello React!</div>;
+};
+
+ReactDOM.render(<App />, document.getElementById("app"));
+
 const $dupsko = jQuery("#get_in_n2");
 const $button_n1 = jQuery("#get_in_n1");
 const changeColor = () => {
